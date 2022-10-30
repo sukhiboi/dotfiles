99e199326144ae60e6754c6889d362c44beaf8cb
sukhiboi <41sukhdevsingh@gmail.com>
2022-10-31 00:30:47 +0530

    update sketchybar plugins

--- sketchybar/plugins/docker_status.sh
+++ sketchybar/plugins/docker_status.sh
@@ -6,14 +6,13 @@ if colima list | grep docker | grep Running > /dev/null 2>&1; then
   BACKGROUND_COLOR="0x9900FF00"
 fi
 
-sketchybar --set $NAME \
+sketchybar --set \
+  $NAME icon="Docker" \
   background.color="$BACKGROUND_COLOR" \
-  icon="Docker" \
   icon.color="0xFFFFFFFF"
-background.drawing="on" \
+  background.drawing="on" \
+  icon.drawing="on" \
   background.padding_left=5 \
   background.padding_right=5 \
   background.corner_radius=5 \
-  label.docker_status=off \
-  label.drawing=off \
-  background.height=22
+  label.drawing="off"
--- sketchybar/plugins/tmux.sh
+++ sketchybar/plugins/tmux.sh
@@ -6,7 +6,7 @@ windowsInAttachedSession=""
 availableSessions=""
 tmuxStatus="Server Down"
 
-if [[ $(tmux has-session 2>&1) != *"no server"* ]]; then
+if [[ $(tmux has-session 2>&1) != *"tmux-501"* ]]; then
   isTmuxRunning=1
   availableSessions=$(tmux ls -F "#{session_id}: #{?session_attached,attached,not attached}" | cut -d ":" -f 1 | cut -d "$" -f2 | xargs)
   attachedSessionId=$(tmux ls -F "#{session_id}: #{?session_attached,attached,not attached}" | grep $(tmux display-message -p '#S') | grep ": attached" | cut -d ":" -f 1)
@@ -26,7 +26,6 @@ sketchybar --set \
   background.padding_left=5 \
   background.padding_right=5 \
   background.corner_radius=5 \
-  label.docker_status=off \
   label.drawing=off \
   icon.color="0xCC000000" \
   background.height=22
