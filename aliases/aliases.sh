371209e3ba35446114c7e9c0dbd3b13c433ab299
sukhiboi <41sukhdevsingh@gmail.com>
2024-01-28 21:46:16 +0530

    Cleaned up dotfiles

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -11,25 +11,9 @@ alias grep="grep --color=always"
 alias tree="tree -x --dirsfirst -CI 'node_modules'"
 alias vz="vim ~/.zshrc"
 alias sz="source ~/.zshrc"
-alias react="npx create-react-app"
 alias p="pwd"
 alias repl="node-prototype-repl"
 alias python="python3"
-alias vim="nvim"
 alias ch="~/bin/cht.sh"
 alias cat="bat"
 
-# less() {
-#   if [[ $1 == *.md ]]
-#   then
-#     glow -p $1
-#   else
-#     /usr/bin/less $1
-#   fi
-# }
-
-# alias less="less"
-
-sonarscan() {
-  sonar-scanner -Dsonar.projectKey=$1 -Dsonar.sources=. -Dsonar.host.url=http://localhost:9000 -Dsonar.login=60110291651d9c7c0912c572d038089ef07d1ffc
-}
--- sketchybar/plugins/wifi.sh
+++
@@ -1,4 +0,0 @@
-WIFI_NAME=$(/Sy*/L*/Priv*/Apple8*/V*/C*/R*/airport -I | awk '/ SSID:/ {print $2}')
-IP_ADDRESS=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
-
-sketchybar --set $NAME label=" $WIFI_NAME $IP_ADDRESS"
